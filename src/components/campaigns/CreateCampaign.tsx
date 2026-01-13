3a881a3f21995829d8ee77fe843234ce5562a5e6
Gonzalo <gonzamaunas@gmail.com>
2026-01-12 23:01:51 -0300

    Agregado grid a campos de crear campaña.

--- src/components/campaigns/CreateCampaign.tsx
+++ src/components/campaigns/CreateCampaign.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete, Container, Paper, TextField, Typography, Button } from "@mui/material"
+import { Autocomplete, Container, Paper, TextField, Typography, Button, Grid } from "@mui/material"
 import { useEffect, useState } from "react"
 import { Controller, useForm } from "react-hook-form"
 import type { Workspace } from "../../types/leads"
@@ -7,7 +7,7 @@ import { createCampaign, getWorkspaces } from "./campaignServices"
 export const CreateCampaign = () => {
 
     return (
-        <Container>
+        <Container >
             <Paper sx={{ p: 2 }}>
                 <Typography variant="h1">
                     Crear Campaña
@@ -29,31 +29,43 @@ export const CampaignForm = () => {
     }, [])
 
     const submit = (data) => {
-        createCampaign(data).then((res)=>{
+        createCampaign(data).then((res) => {
             console.log(res)
-        }).catch((e)=>console.error(e))
+        }).catch((e) => console.error(e))
     }
 
     return (
         <form onSubmit={handleSubmit(submit)}>
+            <Grid container spacing={2} sx={{
+                justifyContent: "center",
+                alignItems: "center",
+                margin:"1rem"
+            }}>
+                <Grid size="grow" minWidth={"20rem"}>
+                    <TextField {...register("name")} label="Nombre" fullWidth  />
+                </Grid>
+                <Grid size="grow" minWidth={"20rem"}>
+                    <TextField {...register("description")} label="Descripción" fullWidth  />
+                </Grid>
+                <Grid size="grow" minWidth={"20rem"}>
+                    <Controller name="workspace_id" control={control}
+                        render={({ field, ...props }) => (
+                            <Autocomplete
+                                {...props}
+                                disablePortal
+                                options={workspaces}
+                                renderInput={(params) => <TextField {...params} label="Workspace" />}
+                                getOptionLabel={(option) => option.name}
+                                getOptionKey={(option) => option.id}
+                                onChange={(_, value) => field.onChange(value?.id)}
+                            />
+                        )}
+                    >
+                    </Controller>
+                </Grid>
+            </Grid>
 
-            <TextField {...register("name")} label="Nombre" />
-            <TextField {...register("description")} label="Descripción" />
-            <Controller name="workspace_id" control={control}
-                render={({ field, ...props }) => (
-                    <Autocomplete
-                        {...props}
-                        disablePortal
-                        options={workspaces}
-                        renderInput={(params) => <TextField {...params} label="Workspace" />}
-                        getOptionLabel={(option) => option.name}
-                        getOptionKey={(option) => option.id}
-                        onChange={(_, value) => field.onChange(value?.id)}
-                    />
-                )}
-            >
-            </Controller>
-            <Button variant="contained" onClick={handleSubmit(submit)}>
+            <Button variant="contained" onClick={handleSubmit(submit)} sx={{marginBlock:"1rem"}}>
                 Guardar Campaña
             </Button>
         </form>
