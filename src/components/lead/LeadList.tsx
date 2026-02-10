7f7001b4938a80c170e641021e4aad2f428d0e7b
Maunas <gonzamaunas@gmail.com>
2026-02-10 02:00:51 -0300

    Agregada paginación.

--- src/components/lead/LeadList.tsx
+++ src/components/lead/LeadList.tsx
@@ -1,22 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import type { Lead } from '../../types/leads'
 import { getLeads } from './leadService'
-import { Button } from '@mui/material'
+import { Button, Pagination } from '@mui/material'
 import { Link } from 'react-router-dom'
+import type { Paginable } from '../../types/common'
 
 export const LeadList = () => {
 
-    const [leads, setLeads] = useState<Lead[]>(null)
+    const [leads, setLeads] = useState<Paginable<Lead[]> | null>(null)
 
     useEffect(() => {
-        getLeads().then(setLeads)
+        getLeads({ only_active: false }).then(setLeads)
     }, [])
 
+    const [page, setPage] = useState<number>(1)
+    const handlePage = (e: React.ChangeEvent<unknown>, value: number) => {
+        if (value === page) return
+        getLeads({ only_active: false, page: value }).then((res) => {
+            setPage(value)
+            setLeads(res)
+        })
+    }
     return (
         <>
-            {leads?.length > 0 &&
-                leads.map(lead=> <Button component={Link} to={`/leads/${lead.id}`}>{`${lead.field_values[0].value} ${lead.field_values[1].value}`}</Button>)
+            {leads?.items?.length > 0 &&
+                leads.items.map(lead => 
+                <Button component={Link} to={`/leads/${lead.id}`} key={lead.id}>
+                    {`${lead.field_values?.[0]?.value} ${lead.field_values?.[1]?.value}`}
+                    </Button>)
             }
+            <Pagination page={page} onChange={handlePage} count={leads?.total_pages} color="secondary" />
         </>
     )
 }
