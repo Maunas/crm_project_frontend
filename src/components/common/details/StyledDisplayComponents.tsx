07c63065ec151877ff5b691193c882d0f218c76a
Gonzalo <gonzamaunas@gmail.com>
2026-04-09 20:59:34 -0300

    Instalada herramienta para ver re-renders

--- src/components/common/details/StyledDisplayComponents.tsx
+++ src/components/common/details/StyledDisplayComponents.tsx
@@ -2,31 +2,32 @@ import { Chip, LinearProgress, type ChipTypeMap } from "@mui/material";
 import type { OverridableComponent } from "@mui/material/OverridableComponent";
 import { alpha, styled } from "@mui/material/styles";
 
-export const CustomChip = styled(Chip)(({ theme, color = "primary", ...props }) => {
-    const OPACITY = 0.2
+const CHIP_OPACITY = 0.2
+const CHIP_SIZES = {
+    "sm": { padding: "0px", gap: "0px", fontSize: ".8rem" },
+    "md": { padding: "4px", gap: "2px", fontSize: ".8rem" },
+    "lg": { padding: "6px", gap: "4px", fontSize: "1rem" },
+    "xl": { padding: "10px 8px", gap: "6px", fontSize: "1rem" }
+}
+
+export const CustomChip = styled(Chip)(({ theme, color = "primary", size = "md" }) => {
     const chipColor = color === "default" ? "contrast" : color //Soluciona una incompatibilidad con el valor "default"
     const paletteColor = theme.palette[chipColor] ?? theme.palette.primary
-    let sizeObject
-    switch (props.size) {
-        case "sm": sizeObject = { padding: "0px", gap: "0px", fontSize: ".8rem" }; break
-        case "lg": sizeObject = { padding: "6px", gap: "4px", fontSize: "1rem" }; break
-        case "xl": sizeObject = { padding: "10px 8px", gap: "6px", fontSize: "1rem" }; break
-        default: sizeObject = { padding: "4px", gap: "2px", fontSize: ".8rem" }
-    }
+    const sizeObject = CHIP_SIZES[size as keyof typeof CHIP_SIZES]
 
     return [{
         fontWeight: 600,
         height: "auto",
         border: "1px solid",
         borderRadius: ".75rem",
-        backgroundColor: alpha(paletteColor.light, OPACITY),
+        backgroundColor: alpha(paletteColor.light, CHIP_OPACITY),
         borderColor: paletteColor.main,
         color: paletteColor.darker,
         ...sizeObject
     },
     //Invierte los tonos en darkmode
     theme.applyStyles('dark', {
-        backgroundColor: alpha(paletteColor.darker, OPACITY),
+        backgroundColor: alpha(paletteColor.darker, CHIP_OPACITY),
         color: paletteColor.lighter,
     })
     ]
--- src/components/hooks/useDebounce.tsx
+++ src/components/hooks/useDebounce.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 /**Asegura un tiempo de "timeout" milisegundos antes de realizar la función.
  * Evita mandar una petición cuando el valor cambia rápidamente, por ejemplo, ante un onChange en un input.
  */
@@ -7,13 +7,16 @@ export const useDebounce = (timeout = 1000) => {
     const idTimeout = useRef<number | undefined>(undefined)
     const [loading, setLoading] = useState<boolean>(false)
 
+    useEffect(() => {
+        return () => clearTimeout(idTimeout.current)
+    }, [])
+
     const debouncedFunction = useCallback((callback: () => Promise<void>) => {
-        setLoading(true)
         clearTimeout(idTimeout.current)
         idTimeout.current = setTimeout(() => {
-            callback().finally(()=>setLoading(false))
-        }, timeout
-        )
+            setLoading(true)
+            callback().finally(() => setLoading(false))
+        }, timeout)
     }, [timeout])
 
     return ({ debouncedFunction, loading })
