eec7d3ae31cc29d269011319c63d3ab2e69c87ba
Gonzalo <gonzamaunas@gmail.com>
2026-05-12 13:18:34 -0300

    Quitado los campos !visible de LeadForm y LeadPartialUpdate

--- src/features/lead/leadList/LeadListPage.tsx
+++ src/features/lead/leadList/LeadListPage.tsx
@@ -263,7 +263,7 @@ export const LeadListPage = () => {
                 }
                 <PaginationComponent {...pageComponentProps} />
             </Stack >
-            <GenericModal idModal="columns_selector" modalProps={modalProps} buttonText="Modificar Columnas" maxWidth="md" showButton={false}>
+            <GenericModal idModal="columns_selector" modalProps={modalProps} buttonText="Modificar Columnas" maxWidth="md" fullWidth showButton={false}>
                 <LeadColumnSelector originalList={leadFields} selectedFieldIds={selectedFieldIds!} handleSelectedFieldIds={handleSelectedFieldIds} handleClose={modalProps.handleClose} showField="name" />
             </GenericModal>
         </Stack>
--- src/features/leadFields/LeadFieldTable.tsx
+++ src/features/leadFields/LeadFieldTable.tsx
@@ -59,7 +59,7 @@ export const LeadFieldTable = ({ campaign, leadFields, updateLeadFields, updateE
                 <Typography variant="h2">Lista de Campos de Lead</Typography>
                 {sortedFields.length > 0 &&
                     <ButtonGroup sx={{ marginLeft: "auto" }}>
-                        <GenericModal modalProps={modalProps} idModal="simulateLead" buttonText='Vista previa de formulario'
+                        <GenericModal modalProps={modalProps} idModal="simulateLead" buttonText='Vista previa de formulario' maxWidth="xl" fullWidth
                             btnProps={{ actionType: "DETAILS", variant: "outlined" }} sx={{ minWidth: "80vw" }} >
                             {campaign &&
                                 <SimulateLeadFormModal campaign={campaign} leadFields={sortedFields} onCancel={modalProps.handleClose} />
