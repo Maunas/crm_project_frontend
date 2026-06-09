b3d0781e35417010f6e933ffc379f37889fc15b1
Gonzalo <gonzamaunas@gmail.com>
2026-06-08 22:09:00 -0300

    Ajustada sombra en light mode

--- src/components/layout/container/GenericPaper.tsx
+++ src/components/layout/container/GenericPaper.tsx
@@ -5,14 +5,22 @@ import { Paper, styled } from '@mui/material'
 const GenericPaper = styled(Paper)(({ theme }) => {
 
     const originalGradient = "var(--Paper-overlay)"
+    const originalShadow = "var(--Paper-shadow)"
 
     return [{
         paddingInline: "2rem",
         paddingBlock: "1.5rem",
         width: "100%"
     },
+    theme.applyStyles("light", {
+        boxShadow: `
+        ${originalShadow},
+        0px 4px 8px ${theme.alpha(theme.palette.contrast[900], .15)}
+        `
+    }),
     theme.applyStyles("dark", {
         border: `1px solid ${theme.palette.divider}`,
+        borderTop: `1px solid ${theme.alpha(theme.palette.common.white, .3)}`,
         backgroundImage: `
         ${originalGradient},
         linear-gradient(180deg, ${theme.alpha(theme.palette.contrast[50], .05)},
