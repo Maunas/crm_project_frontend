3f091539eb67f6602da6844a69091d016a554827
Gonzalo <gonzamaunas@gmail.com>
2026-07-28 02:02:04 -0300

    Ajustados titles dinámicos, y arreglado problema con el title de páginas sin MainLayout

--- src/routing/routeListExports.tsx
+++ src/routing/routeListExports.tsx
@@ -1,18 +1,26 @@
 import { LoginFormPage } from "src/features/auth/LoginForm";
 import { SignupFormPage } from "src/features/auth/SignupForm";
 import { OnboardingPage } from "src/pages/OnboardingPage";
+import { PageTitle } from "./PageTitle";
 import { ROUTE_LIST_OUTLET_PROCESSED, type RouteListProps } from "./routeList";
 
 /** Lista de rutas que se muestran sin Navbar */
-export const ROUTE_LIST_ROOT: RouteListProps[] = [
+const ROUTE_LIST_ROOT: RouteListProps[] = [
     { path: "/login", element: <LoginFormPage />, title: "Iniciar Sesión" },
     { path: "/signup", element: <SignupFormPage />, title: "Crear Cuenta" },
     { path: "/onboarding", element: <OnboardingPage />, title: "Onboarding" },
 ]
 
+/**Agrega un wrapper para usar el hook de title.
+ * Sin eso, no funciona el hook, al estar fuera del RouteProvider */
+export const ROUTE_LIST_ROOT_PROCESSED = ROUTE_LIST_ROOT.map(i => ({
+    ...i,
+    element: <PageTitle title={i.title}>{i.element}</PageTitle>
+}))
+
 export const ROUTE_LIST_OUTLET: RouteListProps[] = ROUTE_LIST_OUTLET_PROCESSED
 
-export const ROUTE_LIST_FULL = [...ROUTE_LIST_ROOT, ...ROUTE_LIST_OUTLET]
+export const ROUTE_LIST_FULL = [...ROUTE_LIST_ROOT_PROCESSED, ...ROUTE_LIST_OUTLET]
 
 export const GLOBAL_NAVBAR = ROUTE_LIST_OUTLET.filter(i => i.globalNavbar)
 export const REGULAR_NAVBAR = ROUTE_LIST_OUTLET.filter(i => i.regularNavbar)
